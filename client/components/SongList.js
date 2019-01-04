bca208cfb9b957267108ca31c91f34a2919aabb3
Isaac <hawkins.isaac@gmail.com>
2019-01-04 08:28:18 -0600

    add delete icons and deleteSong mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,11 +6,17 @@ import { Link } from 'react-router-dom';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+
+  onSongDelete(id){
+    this.props.mutate({ variables: { id } })
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons"onClick={() => this.onSongDelete(id)}>delete</i>
         </li>
       )
     })    
@@ -34,4 +40,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id){
+      id
+    }
+  }
+`;
+
+export default graphql(mutation) (
+  graphql(query)(SongList)
+);
